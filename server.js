0b6762d93b997d12fa351a42ada06dc8e55d36e8
Brandon <brandonbgrayson@gmail.com>
2021-01-16 00:19:39 -0500

    preparing to set up jawsdb

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 
