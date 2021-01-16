6366bb1f92abeba44323d31999be9697d11c11e3
Brandon <brandonbgrayson@gmail.com>
2021-01-16 03:37:41 -0500

    added jaws_db/connection.js file/connected to database

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "Prolific1",
+    database: "DMM_DB"
+  });
+
+  // eslint-disable-next-line prefer-arrow-callback
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+}
+module.exports = connection;
