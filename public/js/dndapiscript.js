d56d3aea29674cd549037f1480183f6a5374c91b
John Hauth <jkhauth@gmail.com>
2021-01-17 10:28:31 -0500

    built out character/game models, add creationtion handlebars file

--- public/js/dndapiscript.js
+++ public/js/dndapiscript.js
@@ -1,43 +1,41 @@
 $.ajax({
-  url: `https://www.dnd5eapi.co/api/races`,
-  method: "GET",
+  url: "https://www.dnd5eapi.co/api/races",
+  method: "GET"
 }).then(function (res) {
   for (let i = 0; i < res.results.length; i++) {
-  $('<option>').attr('id', res.results[i].name).attr('value', res.results[i].name).text(res.results[i].name).appendTo('#raceChoice');
-  }
-});
+$("<option>").attr("id", res.results[i].name).attr("value", res.results[i].name).text(res.results[i].name).appendTo("#raceChoice");
+}});
 
 $.ajax({
-  url: `https://www.dnd5eapi.co/api/classes`,
-  method: "GET",
+  url: "https://www.dnd5eapi.co/api/classes",
+  method: "GET"
 }).then(function (res) {
   for (let i = 0; i < res.results.length; i++) {
-    $('<option>').attr('id', res.results[i].name).attr('value', res.results[i].name).text(res.results[i].name).appendTo('#classChoice');
-}   
-});
+    $("<option>").attr("id", res.results[i].name).attr("value", res.results[i].name).text(res.results[i].name).appendTo("#classChoice");
+}});
 
 $.ajax({
-  url: 'https://www.dnd5eapi.co/api/equipment-categories/simple-weapons',
-  method: "GET",
+  url: "https://www.dnd5eapi.co/api/equipment-categories/simple-weapons",
+  method: "GET"
 }).then(function (res) {
   // console.log(character)
   for (let i = 0; i < res.equipment.length; i++) {
     // console.log(res.starting_equipment[0].equipment.name)
-    $('<option>').attr('id', res.equipment[i].name).attr('value', res.equipment[i].name).text(res.equipment[i].name).appendTo('#simpleweaponChoice');
-    }
-});
+    $("<option>").attr("id", res.equipment[i].name).attr("value", res.equipment[i].name).text(res.equipment[i].name).appendTo("#simpleweaponChoice");
+}});
 
-$('#raceClassBtn').on('click', function () {
+$("#raceClassBtn").on("click", function () {
   event.preventDefault()
-  var nameValue = $('#characterName').val()
-  var raceChoice = $('#raceChoice').val()
-  var classChoice = $('#classChoice').val()
-  var weaponChoice = $('#simpleweaponChoice').val()
+  var nameValue = $("#characterName").val()
+  var raceChoice = $("#raceChoice").val()
+  var classChoice = $("#classChoice").val()
+  var weaponChoice = $("#simpleweaponChoice").val()
 
-  if (nameValue === ''){
-    alert('Please Enter a Name')
+  if (nameValue === ""){
+    alert("Please Enter a Name")
     return
   }
+
   let character = {
     characterName: nameValue,
     race: raceChoice,
@@ -55,10 +53,10 @@ $('#raceClassBtn').on('click', function () {
   var d100;
   var attribValue
 
-  $('<h1>').text($('#characterName').val()).appendTo('#createdCharacter')
-  $('<h2>').text($('#raceChoice').val()).appendTo('#createdCharacter')
-  $('<h2>').text($('#classChoice').val()).appendTo('#createdCharacter')
-  $('<h2>').text($('#simpleweaponChoice').val()).appendTo('#createdCharacter')
+  $("<h1>").text($("#characterName").val()).appendTo("#createdCharacter")
+  $("<h2>").text($("#raceChoice").val()).appendTo("#createdCharacter")
+  $("<h2>").text($("#classChoice").val()).appendTo("#createdCharacter")
+  $("<h2>").text($("#simpleweaponChoice").val()).appendTo("#createdCharacter")
   for (counter=0;counter<attributesArrayLength;counter++) {
     totalRoll=0;
     for (rollNumber=0;rollNumber<3;rollNumber++) {
@@ -69,4 +67,4 @@ $('#raceClassBtn').on('click', function () {
     attribValue=totalRoll + (d100 * 0.01)
     document.getElementById(attributesArray[counter]).textContent=attributesArray[counter] + ":" + attribValue.toFixed(0);
     }
-})
+});
