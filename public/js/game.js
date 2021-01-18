7e88a9b0139c81452214b6d89a0ebc3fac858566
Brandon <brandonbgrayson@gmail.com>
2021-01-18 18:08:15 -0500

    successfully created newGame object

--- public/js/game.js
+++ public/js/game.js
@@ -2,5 +2,16 @@ $(document).ready(function () {
   $("#gameForm").on("click", function(event) {
       event.preventDefault()
       console.log("gamebtn clicked");
+    // make a new game object
+    let newGame = {
+      name_of_game: $("#nameOfGame").val().trim(),
+      story_line: $("#storyLine").val().trim()
+    }
+    console.log(newGame)
+
+      // create post request for 
+    // $.("/api/game", function (req,res) {
+
+    // })
   })
 });
