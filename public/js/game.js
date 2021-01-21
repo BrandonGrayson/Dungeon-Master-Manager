f061efef58c7139f9ccda88c91e0f355d446da1d
Brandon <brandonbgrayson@gmail.com>
2021-01-20 21:38:28 -0500

    map console logs radio btn selected

--- public/js/game.js
+++ public/js/game.js
@@ -1,8 +1,12 @@
 // eslint-disable-next-line prefer-arrow-callback
   // eslint-disable-next-line prefer-arrow-callback
-  $("#gameForm").on("click", function(event) {
+  $("#gameForm").on("submit", function(event) {
     event.preventDefault();
     console.log("gamebtn clicked");
+    console.log('THE EVENT ----> ', event)
+    let map = $('input[name="mapOfGame"]:checked').val();
+    console.log(map);
+    // check to figure out which map id was selected
     // make a new game object
     const newGame = {
       // eslint-disable-next-line camelcase
@@ -13,14 +17,14 @@
       story_line: $("#storyLine")
         .val()
         .trim(),
-      map_id: $("#mapOfGame").val().trim()
+      map_id: map
     };
-    console.log(newGame);
+    // console.log(newGame);
 
     // create post request for
     $.post("/api/new", newGame).then(() => {
       console.log("Game data was sent!");
     });
-    location.reload();
+    // location.reload();
   });
 
