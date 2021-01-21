5fa52cd812278315a34ecbc44f796010d5d2c562
Brandon <brandonbgrayson@gmail.com>
2021-01-21 09:28:42 -0500

    game btn adds a map

--- public/js/game.js
+++ public/js/game.js
@@ -2,10 +2,11 @@
   // eslint-disable-next-line prefer-arrow-callback
 
 
-  $("#gameForm").on("submit", async function(event) {
+  $("#gameForm").on("submit", function(event) {
     event.preventDefault();
     console.log("gamebtn clicked");
     console.log('THE EVENT ----> ', event)
+
     let map = $('input[name="mapOfGame"]:checked').val();
     console.log(map);
     // check to figure out which map id was selected
@@ -19,7 +20,7 @@
       story_line: $("#storyLine")
         .val()
         .trim(),
-      map_id: await map
+      map_id: map
     };
     console.log("NEW GAME VARIABLE--->", newGame);
 
